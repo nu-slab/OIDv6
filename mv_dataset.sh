8af327eeaa06201f2b415e8516c54ec43ba05df2
yarakiB4 <classziozx@gmail.com>
2022-06-09 16:08:30 +0900

    add output cls_list

--- mv_dataset.sh
+++ mv_dataset.sh
@@ -1,8 +1,10 @@
 #! /bin/zsh
-echo 引数の数:$#
+#echo 引数の数:$#
+rm -rf cls_list.txt
+touch cls_list.txt
 for i in `seq 3 $#`
 do                                                                     
-    echo $@[$i]
+    echo $@[$i] >> cls_list.txt
     mkdir ./download_repo/$@[2]/$@[$i]/images
     mv ./download_repo/$@[2]/$@[$i]/*.jpg ./download_repo/$@[2]/$@[$i]/images/.
 done                                                                   
